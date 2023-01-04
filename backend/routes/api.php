130f7895d544c06d1247e6031870d99d95f16b5d
Musa Balkı <musabalkimb@gmail.com>
2023-01-04 22:42:42 +0300

    Add basic api example

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,10 +17,24 @@ use App\Http\Controllers\JobController;
 */
 
 
-Route::post('/auth/register',[AuthController::class,'createUser']);
-Route::post('/auth/login',[AuthController::class,'loginUser']);
 
-Route::get('/jobs',[JobController::class,'index'])->middleware('auth:sanctum');
+
+Route::resource('products',ProductController::class);
+Route::get('/products/search/{name}',[ProductController::class,'search']);
+
+
+//Route::get('/products',[ProductController::class,'index']);
+//Route::post('/products',[ProductController::class,'store']);
+//Route::get('/products/{id}',[ProductController::class,'show']);
+
+
+//Route::post('/auth/register',[AuthController::class,'createUser']);
+//Route::post('/auth/login',[AuthController::class,'loginUser']);
+//Route::get('/jobs',[JobController::class,'index'])->middleware('auth:sanctum');
+
+
+
+
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
