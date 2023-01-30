7171e7f7c85a00fddaecf2b199d0ee68168d2860
musabalki <musabalkimb@gmail.com>
2023-01-30 23:53:20 +0300

    Add workingtype router

--- frontend/src/components/WorkingType.vue
+++ frontend/src/components/WorkingType.vue
@@ -2,10 +2,9 @@
     <div class="bg-white p-6 rounded-lg">
         <h1 class="text-gray-800 font-medium text-lg mb-2">TYPE OF WORK</h1>
         <div class="flex flex-col space-y-2">
-            <a href="" class="text-gray-600 text-sm">Freelance</a>
-            <a href="" class="text-gray-600 text-sm">Full Time</a>
-            <a href="" class="text-gray-600 text-sm">Part Time</a>
-            <a href="" class="text-gray-600 text-sm">Intern</a>
+            <router-link to="/frelance" class="text-gray-600 text-sm">Freelance</router-link>
+            <router-link to="/full-time" class="text-gray-600 text-sm">Full Time</router-link>
+            <router-link to="/part-time" class="text-gray-600 text-sm">Part Time</router-link>
         </div>
     </div>
 </template>
